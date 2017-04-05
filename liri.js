969c0ec9b04999c20e3149dd7e0b48b32af2a0cc
Patrick <pbentley07@gmail.com>
2017-04-05 12:51:11 -0400

    Closes # :Null
    Why:
    
    *
    
    This change addresses the need by:
    Adds Spotify functionalit to LIRI. Needs to add user choice via process.argv
    *

--- liri.js
+++ liri.js
@@ -7,17 +7,30 @@ var method = process.argv[2];
 
 
 var client = new Twitter(keys.twitterKeys);
+
+if (method === 'twitter'){
  
 var params = {screen_name: 'PI_Bentley', count:20};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error && method === 'twitter') {
-    
-    /*console.log(response.created_at);*/
-    for (var i = 0; i < tweets.length; i ++){
-        console.log(tweets[i].created_at);
-    }
-  } 
-  else {
-      console.log(error)
-  }
-});
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i ++){
+                console.log(tweets[i].created_at);
+            }
+        } 
+        else {
+            console.log(error)
+        }
+    });
+}
+
+if (method === 'spotify-this-song'){
+    spotify.search({type:'track', query: 'can you feel it'}, function(err,data){
+      if(err){
+          console.log(err);
+          return;
+      }
+      else{
+         console.log(JSON.stringify(data));
+      }
+    });
+}
