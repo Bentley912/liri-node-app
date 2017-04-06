3eb512c039545b91e42badfccbc378a63e096346
Patrick <pbentley07@gmail.com>
2017-04-05 22:50:41 -0400

    Closes # : null Why: added omdb functionality *
    This change addresses the need by:
    
    *

--- liri.js
+++ liri.js
@@ -4,17 +4,20 @@ var request = require("request");
 var keys = require("./keys.js");
 
 var method = process.argv[2];
+var media = process.argv[3];    
 
 
 var client = new Twitter(keys.twitterKeys);
 
-if (method === 'twitter'){
+if (method === 'my-tweets'){
  
 var params = {screen_name: 'PI_Bentley', count:20};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
             for (var i = 0; i < tweets.length; i ++){
                 console.log(tweets[i].created_at);
+                console.log(tweets[i].text);
+                console.log("\n-------------\n");
             }
         } 
         else {
@@ -24,13 +27,35 @@ var params = {screen_name: 'PI_Bentley', count:20};
 }
 
 if (method === 'spotify-this-song'){
-    spotify.search({type:'track', query: 'can you feel it'}, function(err,data){
+    spotify.search({type:'track', query: media} , function(err,data){
       if(err){
           console.log(err);
           return;
       }
       else{
-         console.log(JSON.stringify(data));
-      }
+        
+            console.log(JSON.stringify(data, null, 2));
+            
+          }   
     });
+}
+
+if (method === 'movie-this'){
+    var search ='';
+
+for (var i = 3; i < process.argv.length; i ++ ){
+    search += process.argv[i]+ ' '; 
+}
+
+search = search.trim();
+
+
+request("http://www.omdbapi.com/?t="+search + "&y=&plot=short&r=json", function(error, response, body){
+
+    if (!error && response.statusCode === 200){
+        
+        console.log(JSON.parse(body).Year);
+    }
+
+});
 }
