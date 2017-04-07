12f5ceb91c1251a4a9695ca8161a8267b7c56479
Patrick <pbentley07@gmail.com>
2017-04-07 16:03:11 -0400

    Closes # : movie-this and do-what-it-says assignment Why:
    *
    
    This change addresses the need by:
    added functionality for requirements. Needs tweeking for movie-this if statemnt
    *

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ var Twitter = require("twitter");
 var spotify = require("spotify");
 var request = require("request");
 var keys = require("./keys.js");
+var fs = require("fs");
 
 var method = process.argv[2];
 var media = process.argv[3];    
@@ -26,36 +27,89 @@ var params = {screen_name: 'PI_Bentley', count:20};
     });
 }
 
-if (method === 'spotify-this-song'){
+if (method === 'spotify-this-song' && media !== ''){
     spotify.search({type:'track', query: media} , function(err,data){
       if(err){
           console.log(err);
           return;
       }
-      else{
-        
-            console.log(JSON.stringify(data, null, 2));
-            
-          }   
+            for (var i = 0; i < data.tracks.items.length;i++ ){
+                console.log(data.tracks.items[i].artists[0].name);
+                console.log(data.tracks.items[i].name);
+                console.log(data.tracks.items[i].preview_url);
+                console.log(data.tracks.items[i].album.name);
+                console.log('\n-----------------------\n')
+            }     
     });
 }
 
-if (method === 'movie-this'){
-    var search ='';
-
-for (var i = 3; i < process.argv.length; i ++ ){
-    search += process.argv[i]+ ' '; 
+if (method === 'spotify-this-song' && media === ''){
+    spotify.search({type:'track', query:"The+Sign" } , function(err,data){
+    if (err){
+        console.log(err)
+    }
+        console.log(data);
+    });
 }
 
-search = search.trim();
+if (method === 'movie-this' && process.argv[3] !== ' '){
+    var search ='';
 
-
-request("http://www.omdbapi.com/?t="+search + "&y=&plot=short&r=json", function(error, response, body){
-
-    if (!error && response.statusCode === 200){
-        
-        console.log(JSON.parse(body).Year);
+    for (var i = 3; i < process.argv.length; i ++ ){
+        search += process.argv[i]+ ' '; 
     }
 
-});
+    search = search.trim();
+
+    request("http://www.omdbapi.com/?t="+search + "&y=&plot=short&r=json", function(error, response, body){
+
+        if (!error && response.statusCode === 200){
+            console.log('\n------------------------\n') 
+            console.log(JSON.parse(body).Title);   
+            console.log(JSON.parse(body).Year);
+            console.log(JSON.parse(body).Country);
+            console.log(JSON.parse(body).Language);
+            console.log(JSON.parse(body).Plot);
+            console.log(JSON.parse(body).Actors);
+            console.log(JSON.parse(body).Rating);
+            console.log(JSON.parse(body).imdbRating);
+            console.log('\n------------------------\n') 
+        }
+    });
+}
+
+if (method === 'movie-this' && process.argv[3] === ' '){
+     request("http://www.omdbapi.com/?t=Mr.Nobody+&y=&plot=short&r=json", function(error, response, body){
+
+        if (!error && response.statusCode === 200){
+            console.log('\n------------------------\n') 
+            console.log(JSON.parse(body).Title);   
+            // console.log(JSON.parse(body).Year);
+            // console.log(JSON.parse(body).Country);
+            // console.log(JSON.parse(body).Language);
+            // console.log(JSON.parse(body).Plot);
+            // console.log(JSON.parse(body).Actors);
+            // console.log(JSON.parse(body).Rating);
+            // console.log(JSON.parse(body).imdbRating);
+            console.log('\n------------------------\n') 
+        }
+    });
+}
+
+if (method === 'do-what-it-says'){
+   var text = fs.readFileSync('./random.txt','utf8')
+    var res = text.split(',');
+    spotify.search({type:'track', query: res[1]} , function(err,data){
+      if(err){
+          console.log(err);
+          return;
+      }
+            for (var i = 0; i < data.tracks.items.length;i++ ){
+                console.log(data.tracks.items[i].artists[0].name);
+                console.log(data.tracks.items[i].name);
+                console.log(data.tracks.items[i].preview_url);
+                console.log(data.tracks.items[i].album.name);
+                console.log('\n-----------------------\n')
+            }     
+    });
 }
